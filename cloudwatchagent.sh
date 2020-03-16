d670b8a49ed7ff81c6cfad920a5b66cea8c14fdc
“Ripan <halder.r@husky.neu.edu>
2020-03-16 05:27:57 -0400

    Adding cloudwatch agents

---
+++ cloudwatchagent.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install wget
+
+wget https://s3."${aws_region}".amazonaws.com/amazoncloudwatch-agent-"${aws_region}"/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
